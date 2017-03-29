d48a36ce42e966334de4d075069e277b7515cd9b
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-03-28 19:10:51 -0500

    Link user to articles

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,14 @@
 require "rails_helper"
 
 RSpec.feature "Creating Articles" do
+
+  before do
+    @joe = User.create(email: "joe@example.com", password: "password")
+
+    # buit in method provided by Warden::Test::Helpers. See rails_helper
+    login_as(@joe)
+  end
+
   scenario "A user creates a new article" do
     visit "/"
 
@@ -11,7 +19,9 @@ RSpec.feature "Creating Articles" do
 
     click_button "Create Article"
 
+    expect(Article.last.user).to eq(@joe)
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Created by: #{@joe.email}")
   end
 end
