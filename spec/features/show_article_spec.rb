2b32d2545367fed12a1f819d2a614e846b54d4be
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-03-29 08:45:19 -0500

    Hide edit and delete buttons from non-signed in users and non-authors

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.feature "Show Article" do
 
   before do
-    joe = User.create(email: "joe@example.com", password: "password")
-    login_as(joe)
-    @article = Article.create(title: "Title one", body: "Body of article one", user: joe)
+    @joe = User.create(email: "joe@example.com", password: "password")
+    @lisa = User.create(email: "lisa@emaple.com", password: "password")
+    @article = Article.create(title: "Title one", body: "Body of article one", user: @joe)
   end
 
-  scenario "a user can see a specific article" do
+  scenario "hide Edit and Delete buttons to non-signed in users" do
     visit "/"
 
     click_link(@article.title)
@@ -16,5 +16,20 @@ RSpec.feature "Show Article" do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "hide Edit and Delete buttons to non-authors" do
+    login_as(@lisa)
+    visit "/"
+
+    click_link(@article.title)
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
 end
